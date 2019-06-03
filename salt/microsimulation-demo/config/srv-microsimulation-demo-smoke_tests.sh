c44913e35f3826c733e35ea079c4481c277cb1e2
Graham Nott <gnott@starglobal.ca>
2019-06-03 16:58:09 -0700

    More volumes, valid articles.

--- salt/microsimulation-demo/config/srv-microsimulation-demo-smoke_tests.sh
+++ salt/microsimulation-demo/config/srv-microsimulation-demo-smoke_tests.sh
@@ -20,7 +20,11 @@ echo "Loading each issue page..."
 [ $(curl -v http://localhost/collections/4-2/volume-4-2-autumn-2011 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/collections/5-1/volume-5-1-spring-2012 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/collections/5-2/volume-5-2-autumn-2012 -o /dev/null -w '%{http_code}') == 200 ]
+[ $(curl -v http://localhost/collections/6-1/volume-6-1-spring-2013 -o /dev/null -w '%{http_code}') == 200 ]
+[ $(curl -v http://localhost/collections/6-2/volume-6-2-autumn-2013 -o /dev/null -w '%{http_code}') == 200 ]
+[ $(curl -v http://localhost/collections/6-3/volume-6-3-winter-2013 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/collections/7-1/volume-7-1-spring-2014 -o /dev/null -w '%{http_code}') == 200 ]
+[ $(curl -v http://localhost/collections/7-2/volume-7-2-autumn-2014 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/collections/8-1/volume-8-1-spring-2015 -o /dev/null -w '%{http_code}') == 200 ]
 
 echo "Loading article pages..."
@@ -34,17 +38,18 @@ echo "Loading article pages..."
 [ $(curl -v http://localhost/articles/00008 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/articles/00009 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/articles/00010 -o /dev/null -w '%{http_code}') == 200 ]
+
 [ $(curl -v http://localhost/articles/00011 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/articles/00012 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/articles/00013 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/articles/00014 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/articles/00015 -o /dev/null -w '%{http_code}') == 200 ]
-# 00016 currently is not valid
-# [ $(curl -v http://localhost/articles/00016 -o /dev/null -w '%{http_code}') == 200 ]
+[ $(curl -v http://localhost/articles/00016 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/articles/00017 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/articles/00018 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/articles/00019 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/articles/00020 -o /dev/null -w '%{http_code}') == 200 ]
+
 [ $(curl -v http://localhost/articles/00021 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/articles/00022 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/articles/00023 -o /dev/null -w '%{http_code}') == 200 ]
@@ -55,6 +60,7 @@ echo "Loading article pages..."
 [ $(curl -v http://localhost/articles/00028 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/articles/00029 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/articles/00030 -o /dev/null -w '%{http_code}') == 200 ]
+
 [ $(curl -v http://localhost/articles/00031 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/articles/00032 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/articles/00033 -o /dev/null -w '%{http_code}') == 200 ]
@@ -65,49 +71,77 @@ echo "Loading article pages..."
 [ $(curl -v http://localhost/articles/00038 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/articles/00039 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/articles/00040 -o /dev/null -w '%{http_code}') == 200 ]
+
 [ $(curl -v http://localhost/articles/00041 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/articles/00042 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/articles/00043 -o /dev/null -w '%{http_code}') == 200 ]
-# 00044 currently is not valid
-# [ $(curl -v http://localhost/articles/00044 -o /dev/null -w '%{http_code}') == 200 ]
+[ $(curl -v http://localhost/articles/00044 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/articles/00045 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/articles/00046 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/articles/00047 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/articles/00048 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/articles/00049 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/articles/00050 -o /dev/null -w '%{http_code}') == 200 ]
+
 [ $(curl -v http://localhost/articles/00051 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/articles/00052 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/articles/00053 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/articles/00054 -o /dev/null -w '%{http_code}') == 200 ]
+[ $(curl -v http://localhost/articles/00055 -o /dev/null -w '%{http_code}') == 200 ]
+[ $(curl -v http://localhost/articles/00056 -o /dev/null -w '%{http_code}') == 200 ]
+[ $(curl -v http://localhost/articles/00057 -o /dev/null -w '%{http_code}') == 200 ]
+[ $(curl -v http://localhost/articles/00058 -o /dev/null -w '%{http_code}') == 200 ]
+[ $(curl -v http://localhost/articles/00059 -o /dev/null -w '%{http_code}') == 200 ]
+[ $(curl -v http://localhost/articles/00060 -o /dev/null -w '%{http_code}') == 200 ]
 
+[ $(curl -v http://localhost/articles/00061 -o /dev/null -w '%{http_code}') == 200 ]
+[ $(curl -v http://localhost/articles/00062 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/articles/00063 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/articles/00064 -o /dev/null -w '%{http_code}') == 200 ]
-# 00065 currently is not valid
-# [ $(curl -v http://localhost/articles/00065 -o /dev/null -w '%{http_code}') == 200 ]
+[ $(curl -v http://localhost/articles/00065 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/articles/00066 -o /dev/null -w '%{http_code}') == 200 ]
-# 00067 currently is not valid
-# [ $(curl -v http://localhost/articles/00067 -o /dev/null -w '%{http_code}') == 200 ]
+[ $(curl -v http://localhost/articles/00067 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/articles/00068 -o /dev/null -w '%{http_code}') == 200 ]
-# 00069 currently is not valid
-# [ $(curl -v http://localhost/articles/00069 -o /dev/null -w '%{http_code}') == 200 ]
-# 00070 currently is not valid
-# [ $(curl -v http://localhost/articles/00070 -o /dev/null -w '%{http_code}') == 200 ]
-# 00071 currently is not valid
-# [ $(curl -v http://localhost/articles/00071 -o /dev/null -w '%{http_code}') == 200 ]
+[ $(curl -v http://localhost/articles/00069 -o /dev/null -w '%{http_code}') == 200 ]
+[ $(curl -v http://localhost/articles/00070 -o /dev/null -w '%{http_code}') == 200 ]
+
+[ $(curl -v http://localhost/articles/00071 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/articles/00072 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/articles/00073 -o /dev/null -w '%{http_code}') == 200 ]
+[ $(curl -v http://localhost/articles/00074 -o /dev/null -w '%{http_code}') == 200 ]
+[ $(curl -v http://localhost/articles/00075 -o /dev/null -w '%{http_code}') == 200 ]
+[ $(curl -v http://localhost/articles/00076 -o /dev/null -w '%{http_code}') == 200 ]
+[ $(curl -v http://localhost/articles/00077 -o /dev/null -w '%{http_code}') == 200 ]
+[ $(curl -v http://localhost/articles/00078 -o /dev/null -w '%{http_code}') == 200 ]
+[ $(curl -v http://localhost/articles/00079 -o /dev/null -w '%{http_code}') == 200 ]
+[ $(curl -v http://localhost/articles/00080 -o /dev/null -w '%{http_code}') == 200 ]
 
-# 00091 currently is not valid
-# [ $(curl -v http://localhost/articles/00091 -o /dev/null -w '%{http_code}') == 200 ]
+[ $(curl -v http://localhost/articles/00081 -o /dev/null -w '%{http_code}') == 200 ]
+[ $(curl -v http://localhost/articles/00082 -o /dev/null -w '%{http_code}') == 200 ]
+[ $(curl -v http://localhost/articles/00083 -o /dev/null -w '%{http_code}') == 200 ]
+[ $(curl -v http://localhost/articles/00084 -o /dev/null -w '%{http_code}') == 200 ]
+[ $(curl -v http://localhost/articles/00085 -o /dev/null -w '%{http_code}') == 200 ]
+[ $(curl -v http://localhost/articles/00086 -o /dev/null -w '%{http_code}') == 200 ]
+[ $(curl -v http://localhost/articles/00087 -o /dev/null -w '%{http_code}') == 200 ]
+[ $(curl -v http://localhost/articles/00088 -o /dev/null -w '%{http_code}') == 200 ]
+# 00089 currently is not valid
+# [ $(curl -v http://localhost/articles/00089 -o /dev/null -w '%{http_code}') == 200 ]
+[ $(curl -v http://localhost/articles/00090 -o /dev/null -w '%{http_code}') == 200 ]
+
+[ $(curl -v http://localhost/articles/00091 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/articles/00092 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/articles/00093 -o /dev/null -w '%{http_code}') == 200 ]
-# 00094 currently is not valid
-# [ $(curl -v http://localhost/articles/00094 -o /dev/null -w '%{http_code}') == 200 ]
+[ $(curl -v http://localhost/articles/00094 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/articles/00095 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/articles/00096 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/articles/00097 -o /dev/null -w '%{http_code}') == 200 ]
+[ $(curl -v http://localhost/articles/00098 -o /dev/null -w '%{http_code}') == 200 ]
+[ $(curl -v http://localhost/articles/00099 -o /dev/null -w '%{http_code}') == 200 ]
+[ $(curl -v http://localhost/articles/00100 -o /dev/null -w '%{http_code}') == 200 ]
 
+[ $(curl -v http://localhost/articles/00101 -o /dev/null -w '%{http_code}') == 200 ]
+[ $(curl -v http://localhost/articles/00102 -o /dev/null -w '%{http_code}') == 200 ]
+[ $(curl -v http://localhost/articles/00103 -o /dev/null -w '%{http_code}') == 200 ]
 
 [ $(curl -v http://localhost/articles/00110 -o /dev/null -w '%{http_code}') == 200 ]
 [ $(curl -v http://localhost/articles/00111 -o /dev/null -w '%{http_code}') == 200 ]
